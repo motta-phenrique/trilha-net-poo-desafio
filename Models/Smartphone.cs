e224c948609ba07d85cc32748747225f037bd33a
Paulo H Motta <paulomotta@customcode.com.br>
2024-10-03 17:22:33 -0300

    desafio de projeto

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,11 +4,18 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        protected string Modelo {get; set;}
 
-        public Smartphone(string numero)
+        public string Imei { get; set; }
+
+        public int Memoria { get; set; }
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia("999179636", "Nokia 10", "12345678", 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+
+Smartphone iphone = new Iphone("2313123123", "Iphone XR", "2222222", 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Whatsapp");
